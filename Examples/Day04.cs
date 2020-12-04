e11103ed7975767b8c803edbe20f88658584fd17
Daniel Bauer <daniel.bauer.x@gmail.com>
2020-12-04 11:08:26 +0100

    Add Day04/Ex02

--- Examples/Day04.cs
+++ Examples/Day04.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Examples
 {
@@ -21,6 +20,19 @@ namespace AdventOfCode.Examples
 
             return true;
         }
+
+        public static string WithoutPrefix(this string value, string prefix)
+            => value.EndsWith(prefix) ? value.Substring(0, value.Length - prefix.Length) : value;
+
+        public static bool Between(this string str, int min, int max)
+        {
+            return int.TryParse(str, out int result)
+                ? result >= min && result <= max
+                : false;
+        }
+
+        public static bool Matches(this string str, string regex)
+            => new Regex(regex).IsMatch(str);
     }
 
     public static class Day04
@@ -28,25 +40,72 @@ namespace AdventOfCode.Examples
         private static Dictionary<string, string> ToPassport(string value)
         {
             return value
-                .Split(" ")
-                .Select(prop => prop.Split(":"))
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(prop => prop.Split(":", StringSplitOptions.RemoveEmptyEntries))
                 .ToDictionary(key => key[0], value => value[1]);
         }
 
-        public static void Example01()
+        private static IEnumerable<Dictionary<string, string>> GetPassports()
         {
-            var passports = File.ReadAllText("Data/day04.txt")
+            return File.ReadAllText("Data/day04.txt")
                 .Split(Environment.NewLine + Environment.NewLine)
                 .Select(str => str.Replace(Environment.NewLine, " "))
                 .Select(ToPassport)
                 .ToList();
+        }
 
+        public static void Example01()
+        {
+            var passports = GetPassports();
             var requiredFields = new[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
             var validPassports = passports
                 .Where(p => p.Keys.ContainsAll(requiredFields))
                 .Count();
 
             Console.WriteLine(validPassports);
+        }
+
+        private static bool IsHeight(string value)
+        {
+            if (value.EndsWith("cm")) return value.WithoutPrefix("cm").Between(150, 193);
+            if (value.EndsWith("in")) return value.WithoutPrefix("in").Between(59, 76);
+            return false;
+        }
+
+        private static bool ValidateRule(Dictionary<string, string> values, Dictionary<string, Func<string, bool>> rules)
+        {
+            foreach (var value in values)
+            {
+                var rule = rules.GetValueOrDefault(value.Key, null);
+                if (rule != null && !rule(value.Value))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static void Example02()
+        {
+            var requiredFields = new[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+            var rules = new Dictionary<string, Func<string, bool>>
+            {
+                { "byr", value => value.Between(1920, 2002) },
+                { "iyr", value => value.Between(2010, 2020) },
+                { "eyr", value => value.Between(2020, 2030) },
+                { "hgt", value => IsHeight(value) },
+                { "hcl", value => value.Matches("#[a-f0-9]{6}") },
+                { "ecl", value => new[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" }.Contains(value) },
+                { "pid", value => value.Length == 9 && value.Matches("[0-9]{9}") }
+            };
+
+            var validPassports = GetPassports()
+                .Where(p => p.Keys.ContainsAll(requiredFields))
+                .Where(values => ValidateRule(values, rules))
+                .ToList();
+
+            Console.WriteLine(validPassports.Count);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode
         static void Main(string[] args)
         {
             Console.WriteLine("Advent of Code 2020");
-            Day04.Example01();
+            Day04.Example02();
         }
     }
 }
