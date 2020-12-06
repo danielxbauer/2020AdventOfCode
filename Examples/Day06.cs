c67ea021d8d7362690102ec25b551aca9006da5e
Daniel Bauer <daniel.bauer.x@gmail.com>
2020-12-06 12:05:45 +0100

    Add day06

---
+++ Examples/Day06.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Examples
+{
+    public static class Day06
+    {
+
+        public static void Example01()
+        {
+            var questions = File.ReadAllText("Data/day06.txt")
+                .Split(Environment.NewLine + Environment.NewLine)
+                .Select(g => g
+                    .Replace(Environment.NewLine, string.Empty)
+                    .ToHashSet())
+                .Sum(g => g.Count);
+
+            Console.WriteLine(questions);
+        }
+
+        public static void Example02()
+        {
+            var questions = File.ReadAllText("Data/day06.txt")
+               .Split(Environment.NewLine + Environment.NewLine)
+               .Select(g => g.Split(Environment.NewLine))
+               .Select(g => g.Skip(1)
+                    .Aggregate(
+                        g.First().ToList(),
+                        (h, e) => h.Intersect(e.ToList()).ToList()))
+               .Sum(c => c.Count);
+
+            Console.WriteLine(questions);
+        }
+    }
+}
